5eaeb5ce7b0d3c4cccb9ee497bf945c9b5333878
Alexis Renderos <renderosalexis17@gmail.com>
2022-05-05 13:49:22 -0700

    Create workExperienceTimeline component

--- web/components/workExperienceTimeline.tsx
+++ web/components/workExperienceTimeline.tsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-import { WorkExperienceEntry } from "./workExperienceCard";
+import {
+  WorkExperienceCard,
+  WorkExperienceCardPosition,
+  WorkExperienceEntry,
+} from "./workExperienceCard";
+import simpleHash from "../helpers/simpleHash";
 
 export const WorkExperienceCardCollectionWrapper = styled.div`
-  display: inline-flex;
-  flex-direction: row;
+  display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
-  padding: 1rem;
-  margin: 0.5rem;
-  border: 1px solid #0b0b0b;
-  border-radius: 0.75rem;
 `;
 
 export type WorkExperienceCardCollectionProps = {
@@ -17,6 +18,43 @@ export type WorkExperienceCardCollectionProps = {
 
 export const WorkExperienceTimeline = ({
   workExperienceList,
-}: WorkExperienceCardCollectionProps) => (
-  <WorkExperienceCardCollectionWrapper />
-);
+}: WorkExperienceCardCollectionProps): JSX.Element => {
+  const experienceListLength = workExperienceList.length;
+
+  if (!workExperienceList || experienceListLength === 0) {
+    return {} as JSX.Element;
+  }
+
+  return (
+    <WorkExperienceCardCollectionWrapper>
+      {workExperienceList.map((workExperience, index) => {
+        let currentCardPosition: number;
+        if (index === 0) {
+          currentCardPosition = WorkExperienceCardPosition.First;
+        } else if (index === experienceListLength - 1) {
+          currentCardPosition = WorkExperienceCardPosition.Last;
+        } else {
+          currentCardPosition = WorkExperienceCardPosition.Body;
+        }
+        return (
+          <WorkExperienceCard
+            workExperience={workExperience}
+            cardPosition={currentCardPosition}
+            key={simpleHash(
+              workExperience.company + workExperience.title + index
+            )}
+          />
+        );
+      })}
+    </WorkExperienceCardCollectionWrapper>
+  );
+
+  if (experienceListLength === 1) {
+    return (
+      <WorkExperienceCard
+        workExperience={workExperienceList[0]}
+        cardPosition={WorkExperienceCardPosition.Only}
+      />
+    );
+  }
+};
