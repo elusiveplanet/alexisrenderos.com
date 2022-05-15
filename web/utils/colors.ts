332d838cb3e19c6eae558268ba9b7cec03642a8b
Alexis Renderos <renderosalexis17@gmail.com>
2022-05-14 22:36:13 -0700

    Add colors to utils

---
+++ web/utils/colors.ts
@@ -0,0 +1,29 @@
+// Yellows
+export const SaffronYellow = "rgba(243, 200, 27, 1)";
+
+// Blues
+export const IndigoBlue = "rgba(24, 48, 73, 1)";
+export const NavyBlue = "rgba(24, 48, 73, 1)";
+export const CobaltBlue = "rgba(24, 58, 91, 1)";
+export const AzureBlue = "rgba(24, 81, 135, 1)";
+
+// RGBA Shadows
+export const QuarterShadow = "rgba(0, 0, 0, 0.25)";
+
+// Text Colors
+export const LightText = "rgba(255, 255, 255, 1)";
+export const DarkText = "rgba(0, 0, 0, 1)";
+
+// Background Colors
+export const FullyTransparent = "rgba(0, 0, 0, 0)";
+export const SlightlyDarken = "rgba(0, 0, 0, 0.075)";
+export const ModeratelyDarken = "rgba(0, 0, 0, 0.1)";
+export const SignificantlyDarken = "rgba(0, 0, 0, 0.15)";
+export const SlightlyLighten = "rgba(255, 255, 255, 0.15)";
+export const SignificantlyLighten = "rgba(255, 255, 255, 0.075)";
+
+export const IndigoToAzureGradient = `linear-gradient(to right, 
+  ${IndigoBlue} 0%, 
+  ${NavyBlue} 8%,
+  ${CobaltBlue} 36%,
+  ${AzureBlue} 100%)`;
