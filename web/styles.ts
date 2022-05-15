8e1616c06c96c764a045ba6b69a2c6ad331fdc0c
Alexis Renderos <renderosalexis17@gmail.com>
2022-05-14 22:35:20 -0700

    Add resources, update gitignore

--- web/styles.ts
+++
@@ -1,9 +0,0 @@
-export const pageColors = {
-  primary: '#220a82',
-  secondary: '#14cbc4',
-  accent: 'rgba(243, 200, 27, 1)',
-  background: '#f7f8fa',
-  grey: '#d8d9e0',
-  text: '#343c5a',
-  textSecondary: '#747790'
-};
