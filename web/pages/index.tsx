505e001c938c64654ecca925c1ba54ec988b14fa
Alexis Renderos <renderosalexis17@gmail.com>
2022-05-16 06:53:21 -0700

    Update index to a proper splash screen

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
-import useSWR from "swr";
 import styled from "styled-components";
 import { GlobalStyle } from "../styles/styles";
-import { WorkExperienceTimeline } from "../components/workExperienceTimeline";
-import { ProjectCardCollection } from "../components/projectCardCollection";
+import { DarkText, HalfShadow, SaffronYellow } from "../utils/colors";
 
 const IndexBody = styled.div`
   display: flex;
@@ -11,24 +9,53 @@ const IndexBody = styled.div`
   flex-wrap: nowrap;
 `;
 
-const fetcher = async (
-  input: RequestInfo,
-  init: RequestInit,
-  ...args: any[]
-) => {
-  const res = await fetch(input, init);
-  return res.json();
-};
+export const IndexTitle = styled.h2`
+  font-size: 3em;
+  font-weight: 700;
+  margin: 0.5em 0;
+  text-align: center;
+  color: ${DarkText};
+  background: ${SaffronYellow};
+  border: solid ${SaffronYellow};
+  border-width: 0.175em 0.35em 0.175em 0.35em;
+  box-shadow: 15px 15px 2px 0 ${HalfShadow};
+`;
+
+export const IndexSubtitle = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  h3,
+  h5 {
+    margin: 0.25em 0;
+  }
+`;
+
+const HeroContent = styled.div`
+  width: 65%;
+  margin: 0 auto;
+  max-width: 500px;
+`;
+
+// const fetcher = async (
+//   input: RequestInfo,
+//   init: RequestInit,
+//   ...args: any[]
+// ) => {
+//   const res = await fetch(input, init);
+//   return res.json();
+// };
 
 export const Home = (): JSX.Element => {
-  const { data: workExperienceData, error: workExperienceDataError } = useSWR(
-    "/api/getAllVisibleWorkExperience",
-    fetcher
-  );
-  const { data: projectData, error: projectDataError } = useSWR(
-    "/api/getAllVisibleProjects",
-    fetcher
-  );
+  // const { data: workExperienceData, error: workExperienceDataError } = useSWR(
+  //   "/api/getAllVisibleWorkExperience",
+  //   fetcher
+  // );
+  // const { data: projectData, error: projectDataError } = useSWR(
+  //   "/api/getAllVisibleProjects",
+  //   fetcher
+  // );
+
+  const temp = 1;
 
   return (
     <>
@@ -36,35 +63,49 @@ export const Home = (): JSX.Element => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>Alexis Renderos</title>
       <IndexBody>
-        <h1>Alexis Renderos</h1>
-        <h3>👋 Welcome!</h3>
-        <h5>I&apos;m currently rebuilding my personal website.</h5>
-        <h5>
-          The old version is still at{" "}
-          <Link href="https://renderos17.github.io" passHref>
-            renderos17.github.io
-          </Link>
-          .
-        </h5>
-        <h5>
-          Designs for the new version can be found on{" "}
-          <Link
-            href="https://www.figma.com/file/2zSroQb71YqrjcLDHLGiEh/Final-Website-Mock?node-id=548%3A483"
-            passHref>
-            Figma
-          </Link>
-          .
-        </h5>
+        <HeroContent>
+          <IndexTitle>Alexis Renderos</IndexTitle>
+          <IndexSubtitle>
+            <h3>👋 Welcome!</h3>
+            <h5>I&apos;m currently rebuilding my personal website.</h5>
+            <h5>
+              The old version is still at{" "}
+              <Link href="https://renderos17.github.io" passHref>
+                renderos17.github.io
+              </Link>
+              .
+            </h5>
+            <h5>
+              Designs for the new version can be found on{" "}
+              <Link
+                href="https://www.figma.com/file/2zSroQb71YqrjcLDHLGiEh/Final-Website-Mock?node-id=548%3A483"
+                passHref>
+                Figma
+              </Link>
+              .
+            </h5>
+            <h5>
+              If you&apos;d like to learn more about what I&apos;ve worked on,
+              check out my{" "}
+              <Link
+                href="https://renderos17.github.io/AlexisRenderosResume.pdf"
+                passHref>
+                Resume
+              </Link>
+              .
+            </h5>
+          </IndexSubtitle>
+        </HeroContent>
 
-        {!!workExperienceData && (
-          <WorkExperienceTimeline workExperienceList={workExperienceData} />
-        )}
-        {!!workExperienceDataError && (
-          <p>Error fetching work experience data...</p>
-        )}
+        {/*{!!workExperienceData && (*/}
+        {/*  <WorkExperienceTimeline workExperienceList={workExperienceData} />*/}
+        {/*)}*/}
+        {/*{!!workExperienceDataError && (*/}
+        {/*  <p>Error fetching work experience data...</p>*/}
+        {/*)}*/}
 
-        {!!projectData && <ProjectCardCollection projectList={projectData} />}
-        {!!projectDataError && <p>Error fetching project data...</p>}
+        {/*{!!projectData && <ProjectCardCollection projectList={projectData} />}*/}
+        {/*{!!projectDataError && <p>Error fetching project data...</p>}*/}
       </IndexBody>
     </>
   );
