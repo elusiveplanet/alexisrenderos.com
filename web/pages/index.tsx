0576773b98339d5f06c1ae2a0dbb22906ac5a341
Alexis Renderos <renderosalexis17@gmail.com>
2022-05-16 19:48:31 -0700

    Add favicons :)

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -66,7 +66,48 @@ export const Home = (): JSX.Element => {
         <title>Alexis Renderos 🪐</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <title>Alexis Renderos</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/favicon/site.webmanifest" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://alexisrenderos.com" />
+        <meta property="og:title" content="Alexis Renderos" />
+        <meta
+          property="og:description"
+          content="I enable others to do their best work."
+        />
+        <meta
+          property="og:image"
+          content="https://alexisrenderos.com/images/goldenGateWithDog.jpeg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Alexis Renderos" />
+        <meta
+          name="twitter:description"
+          content="I enable others to do their best work."
+        />
+        <meta
+          name="twitter:image"
+          content="https://alexisrenderos.com/images/goldenGateWithDog.jpeg"
+        />
+        <meta name="twitter:creator" content="@elusiveplanet" />
       </Head>
       <IndexBody>
         <HeroContent>
