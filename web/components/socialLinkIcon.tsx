bbe9bd5137f69f2c1392934dbc42f144e6fb691f
Alexis Renderos <renderosalexis17@gmail.com>
2022-06-05 22:49:26 -0700

    Add size option to socialLink icon

--- web/components/socialLinkIcon.tsx
+++ web/components/socialLinkIcon.tsx
@@ -7,6 +7,12 @@ export enum SocialLinkTarget {
   Twitter = "twitter-nb.png",
 }
 
+export enum SocialLinkSize {
+  Small = 0,
+  Medium = 1,
+  Large = 2,
+}
+
 export type SocialLink = {
   service: SocialLinkTarget;
   url: string;
@@ -14,32 +20,75 @@ export type SocialLink = {
 
 export type SocialLinkIconProps = {
   socialLink: SocialLink;
+  size: SocialLinkSize;
 };
 
 const SocialLinkServiceIcon = styled.div`
-  height: 2em;
-  width: 2em;
-  margin: 0.1em;
-  padding: 0.5em;
   &:hover {
     filter: brightness(60%);
     transition: all 0.5s ease;
   }
 `;
 
-export const SocialLinkIcon = ({ socialLink }: SocialLinkIconProps) => {
+const SocialLinkServiceIconSmall = styled(SocialLinkServiceIcon)`
+  height: 2em;
+  width: 2em;
+  margin: 0.1em;
+  padding: 0.5em;
+`;
+
+const SocialLinkServiceIconMedium = styled(SocialLinkServiceIcon)`
+  height: 2.5em;
+  width: 2.5em;
+  margin: 0.1em;
+  padding: 0.5em;
+`;
+
+const SocialLinkServiceIconLarge = styled(SocialLinkServiceIcon)`
+  height: 3em;
+  width: 3em;
+  margin: 0.1em;
+  padding: 0.5em;
+`;
+
+export const SocialLinkIcon = ({ socialLink, size }: SocialLinkIconProps) => {
   const imageLocation = `/images/${SocialLinkTarget[socialLink.service]}`;
-  return (
-    <SocialLinkServiceIcon>
-      <a href={socialLink.url}>
-        <Image
-          src={imageLocation}
-          layout="responsive"
-          width={40}
-          height={40}
-          alt={`Alexis' ${socialLink.service || "Social Media Profile"}`}
-        />
-      </a>
-    </SocialLinkServiceIcon>
+  const socialLinkChild = (
+    <a href={socialLink.url}>
+      <Image
+        src={imageLocation}
+        layout="responsive"
+        width={40}
+        height={40}
+        alt={`Alexis' ${socialLink.service || "Social Media Profile"}`}
+      />
+    </a>
   );
+
+  switch (size) {
+    case SocialLinkSize.Small:
+      return (
+        <SocialLinkServiceIconSmall>
+          {socialLinkChild}
+        </SocialLinkServiceIconSmall>
+      );
+    case SocialLinkSize.Medium:
+      return (
+        <SocialLinkServiceIconMedium>
+          {socialLinkChild}
+        </SocialLinkServiceIconMedium>
+      );
+    case SocialLinkSize.Large:
+      return (
+        <SocialLinkServiceIconLarge>
+          {socialLinkChild}
+        </SocialLinkServiceIconLarge>
+      );
+    default:
+      return (
+        <SocialLinkServiceIconMedium>
+          {socialLinkChild}
+        </SocialLinkServiceIconMedium>
+      );
+  }
 };
