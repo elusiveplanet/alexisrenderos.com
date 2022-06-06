3eb1c0756d8df2bee8db7043ffc80d2a1cc773bd
Alexis Renderos <renderosalexis17@gmail.com>
2022-06-05 22:49:51 -0700

    Create navbar

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -1,25 +1,71 @@
 import styled from "styled-components";
+import {
+  DarkText,
+  FullyTransparent,
+  HalfShadow,
+  LightText,
+  SaffronYellow,
+} from "../utils/colors";
 
 export type JuiceboxHeaderProps = {
   title: string;
-  value: string | number;
 };
 
-export const JuiceboxHeaderTitle = styled.p`
-  flex: 0 0 100%;
+export const JuiceboxHeaderTitle = styled.h2`
+  font-size: 2.5em;
+  font-weight: 700;
+  margin: 0.5em;
+  text-align: left;
+  color: ${DarkText};
+  background: ${SaffronYellow};
+  border: solid ${SaffronYellow};
+  border-width: 0.175em 0.35em 0.175em 0.35em;
+  box-shadow: 12px 12px 2px 0 ${HalfShadow};
+  width: fit-content;
+`;
+
+export const JuiceboxOption = styled.a`
+  font-size: 2em;
+  font-weight: 500;
+  margin: 0.5em;
+  text-align: right;
+  color: ${LightText};
+  background: ${FullyTransparent};
+  text-decoration-color: ${FullyTransparent};
+  width: max-content;
 `;
 
 const JuiceboxHeaderWrapper = styled.div`
-  margin: 0.1rem 0;
-  padding: 0.1rem 0;
-  width: max-content;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0.5em 0;
+  width: 100%;
+  max-width: 1400px;
   border-radius: 0.75rem;
 `;
 
-export const JuiceboxHeader = ({ title, value }: JuiceboxHeaderProps) => (
+const JuiceboxTitleWrapper = styled.div`
+  //align-items: flex-start;
+  width: 100%;
+`;
+
+const JuiceboxOptionsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  margin: auto;
+  width: max-content;
+`;
+
+export const JuiceboxHeader = ({ title }: JuiceboxHeaderProps) => (
   <JuiceboxHeaderWrapper>
-    <JuiceboxHeaderTitle>
-      {title}: {value || ""}
-    </JuiceboxHeaderTitle>
+    <JuiceboxTitleWrapper>
+      <JuiceboxHeaderTitle>{title}</JuiceboxHeaderTitle>
+    </JuiceboxTitleWrapper>
+    <JuiceboxOptionsWrapper>
+      <JuiceboxOption>About Me</JuiceboxOption>
+      <JuiceboxOption>Get In Touch</JuiceboxOption>
+    </JuiceboxOptionsWrapper>
   </JuiceboxHeaderWrapper>
 );
