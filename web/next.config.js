4eb076de2fe5562f57689f01dde83d832cc0dc0d
Alexis Renderos <renderosalexis17@gmail.com>
2022-06-27 02:33:48 -0700

    Edit next config, update packages

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
   compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: true,
   },
 };
