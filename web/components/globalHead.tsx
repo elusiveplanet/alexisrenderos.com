583ebff71cbab9f237fae7a730682cd0f159a3e4
Alexis Renderos <renderosalexis17@gmail.com>
2022-07-09 00:04:40 -0700

    Add hotjar

--- web/components/globalHead.tsx
+++ web/components/globalHead.tsx
@@ -66,6 +66,19 @@ const GlobalHead = () => (
       content="https://alexisrenderos.com/images/goldenGateWithDog.jpeg"
     />
     <meta name="twitter:creator" content="@elusiveplanet" />
+    
+    <!-- Hotjar Tracking Code for https://alexisrenderos.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3058994,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    
   </Head>
 );
 
