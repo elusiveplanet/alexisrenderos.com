f5a1b847c423265683c363059b0dd07a31be76e6
Alexis Renderos <renderosalexis17@gmail.com>
2022-10-03 15:57:10 -0700

    Rename airtable endpoints and migrate from .netlify

--- web/pages/api/createAirtableFormEntry.ts
+++ web/pages/api/createAirtableFormEntry.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import table from "./airtable";
 
-const createAirtableContactFormEntry = async (
+const createAirtableFormEntry = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
@@ -11,16 +11,16 @@ const createAirtableContactFormEntry = async (
   }
   const fields = req.body;
   try {
-    const createdContactFormEntry = await table.create(
+    const createdFormEntry = await table.create(
       {
         fld1nJIYWqeOUMejq: fields.firstName,
         fldQvofXuCwST8MkS: fields.lastName,
         fldbYKkLmfbOIq8ai: fields.email,
         fldaO1Vlb9XqFLjSB: fields.message,
+        fldomlkL0CfXyCsfm: fields.resRequest ? "RESUME_REQUEST" : "none",
       },
       (err, record) => {
         if (err) {
-          console.error(err);
           throw new Error("Error creating new record");
         }
       }
@@ -28,9 +28,8 @@ const createAirtableContactFormEntry = async (
     res.status(200).send({ message: "Successfully created new record" });
     return;
   } catch (err) {
-    console.error(err);
     res.status(500).send({ message: "Error creating new record" });
   }
 };
 
-export default createAirtableContactFormEntry;
+export default createAirtableFormEntry;
